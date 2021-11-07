3c9c24210b7011a29c8e659bdcb58f9218913779
”Junior” <rip05-04-1990@yandex.ru>
2021-11-07 22:44:57 +0300

    Spring Rest - Exception handler

--- Spring_REST/src/main/java/rest/controller/MyRESTController.java
+++ Spring_REST/src/main/java/rest/controller/MyRESTController.java
@@ -1,8 +1,12 @@
 package rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import rest.entity.Employee;
+import rest.exception_handling.EmployeeIncorrectData;
+import rest.exception_handling.NoSuchEmployeeException;
 import rest.service.EmployeeService;
 
 import java.util.List;
@@ -27,8 +31,29 @@ public class MyRESTController {
 
         Employee employee = employeeService.getEmployee(id);
 
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with ID = " + id + " in Database");
+        }
+
         return employee;
     }
 
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(NoSuchEmployeeException e) {
+
+        EmployeeIncorrectData employeeIncorrectData = new EmployeeIncorrectData();
+        employeeIncorrectData.setInfo(e.getMessage());
+
+        return new ResponseEntity<>(employeeIncorrectData, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(Exception e) {
+
+        EmployeeIncorrectData employeeIncorrectData = new EmployeeIncorrectData();
+        employeeIncorrectData.setInfo(e.getMessage());
+
+        return new ResponseEntity<>(employeeIncorrectData, HttpStatus.BAD_REQUEST);
+    }
 
 }
