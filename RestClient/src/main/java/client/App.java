b8e9c42ce3c6aa18f9ebe9bd8b01fb6f9c1da34d
”Junior” <rip05-04-1990@yandex.ru>
2021-11-08 11:31:26 +0300

    Spring Rest - creating rest client

---
+++ RestClient/src/main/java/client/App.java
@@ -0,0 +1,29 @@
+package client;
+
+import client.configuration.MyConfig;
+import client.entity.Employee;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext configApplicationContext = new AnnotationConfigApplicationContext(MyConfig.class);
+        Communication communication = configApplicationContext.getBean("communication", Communication.class);
+
+//        List<Employee> list = communication.getAllEmployees();
+//        System.out.println(list);
+//
+//        System.out.println(communication.getEmployee(1));
+
+//        Employee employee = new Employee("Sveta", "Sokolova", "IT", 1200);
+//        employee.setId(8);
+        
+        communication.deleteEmployee(8);
+
+
+    }
+}
